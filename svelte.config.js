9dd48a714671f9e3cdf24795d06d85bf8891509d
BEN ELHADJ Hamdi <bhamdi@student.42.fr>
2025-10-07 01:44:24 +0200

    Update Svelte adapter configuration for environments
    Switch adapter based on deployment environment.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: process.env.RENDER
+			? node()       // si déploiement sur Render
+			: vercel()     // sinon Vercel par défaut
 	}
 };
 
